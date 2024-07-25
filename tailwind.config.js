1687870a9966134187c2ddc41bbd46f3e5f055b6
HHazard2002 <hugo7.hazard@gmail.com>
2024-07-25 22:48:30 +0200

    add public file

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        alliance: ["Alliance", "sans-serif"],
+      },
+      animation: {
+        ticker: "ticker 30s infinite linear",
+      },
+      keyframes: {
+        ticker: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
+  },
+  plugins: [
+    addVariablesForColors,
+    
+    require("@tailwindcss/forms")],
+};
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
